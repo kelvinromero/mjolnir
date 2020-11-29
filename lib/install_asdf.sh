f9f2ab22b9f93889fd251eb60ee7d905eef92e71
Kelvin Romero <kelvinromero@gmail.com>
2020-11-29 14:05:00 -0300

    adds asdf

---
+++ lib/install_asdf.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+
+echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
+echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
+
+source ~/.zshrc
+source ~/.bashrc
