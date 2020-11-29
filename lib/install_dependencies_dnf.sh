63fd3fe2a98d53bd44aa655afbde5deafd66fefd
Kelvin Romero <kelvinromero@gmail.com>
2020-11-29 14:07:23 -0300

    adds nodejs yarnpkg gcc sqlite-devel

--- lib/install_dependencies_dnf.sh
+++ lib/install_dependencies_dnf.sh
@@ -1,1 +1,6 @@
-sudo dnf install -y git zsh
+sudo dnf update
+sudo dnf upgrade
+
+dnf module install nodejs:12
+
+sudo dnf install -y git zsh openssl-devel readline-devel zlib-devel nodejs yarnpkg gcc sqlite-devel
