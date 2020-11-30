64036e0a365e6f55b2080841a234a3791c1c1aba
Kelvin Romero <kelvinromero@gmail.com>
2020-11-30 11:24:22 -0300

    Generate ssh key and copies into clipboard

---
+++ lib/generate_ssh_key.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt show xclip
+if [ ! $? -eq 0]; then
+    echo "Installing dependencies"
+    sudo apt install xclip
+fi
+
+if [ -z "$1" ]; then
+    echo "Inform the key name, example:"
+    echo -e "\t $ make ssh-key name=mey-key-name"
+    exit
+fi
+
+if [ ! -f ~/.ssh/id_ed25519.pub ]; then
+    echo "Criando chave SSH"
+    ssh-keygen -t ed25519 -C "$1"
+fi
+
+echo "SSH key copyed!"
+xclip -sel clip <~/.ssh/id_ed25519.pub
