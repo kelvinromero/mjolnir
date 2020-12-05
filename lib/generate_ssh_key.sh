843340b43725fddf37335961e50c82c984e1b84e
Kelvin Romero <kelvinromero@gmail.com>
2020-12-05 12:45:02 -0300

    Check existence before asking name

--- lib/generate_ssh_key.sh
+++ lib/generate_ssh_key.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-sudo apt show xclip > /dev/null 2>&1
-if [ ! $? -eq 0]; then
-    echo "Installing dependencies"
+sudo apt show xclip >/dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+    echo "Installing dependencies..."
     sudo apt install xclip
 fi
 
-if [ -z "$1" ]; then
-    echo "Inform the key name, example:"
-    echo -e "\t $ make ssh-key name=mey-key-name"
-    exit
-fi
-
 if [ ! -f ~/.ssh/id_ed25519.pub ]; then
-    echo "Criando chave SSH"
+    if [ -z "$1" ]; then
+        echo "\nInform the key name, example:"
+        echo -e "\n\t $ make ssh-key name=mey-key-name\n"
+        exit
+    fi
+
+    echo "Creating ssh key..."
     ssh-keygen -t ed25519 -C "$1" -f ~/.ssh/id_ed25519.pub
+else
+    echo "Already exists!"
 fi
 
 echo "SSH key copyed!"
-xclip -sel clip < ~/.ssh/id_ed25519.pub
+xclip -sel clip <~/.ssh/id_ed25519.pub
